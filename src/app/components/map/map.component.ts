37988c673407df0afd7a7834c99c1c0cc5cc81b9
Raphael Luiz Martins Koszalka <rahael@getbyond.com>
2018-06-26 11:00:25 -0300

    MapComponent style array

--- src/app/components/map/map.component.ts
+++ src/app/components/map/map.component.ts
@@ -35,7 +35,70 @@ export class MapComponent implements AfterViewInit {
     const map = new google.maps.Map(mapElm, {
       center: latLng,
       scrollwheel: false,
-      zoom: 17
+      zoom: 17,
+      styles: [
+        {
+          "stylers": [
+            {
+              "saturation": -100
+            }
+          ]
+        },
+        {
+          "featureType": "water",
+          "elementType": "geometry.fill",
+          "stylers": [
+            {
+              "color": "#0099dd"
+            }
+          ]
+        },
+        {
+          "elementType": "labels",
+          "stylers": [
+            {
+              "visibility": "off"
+            }
+          ]
+        },
+        {
+          "featureType": "poi.park",
+          "elementType": "geometry.fill",
+          "stylers": [
+            {
+              "color": "#aadd55"
+            }
+          ]
+        },
+        {
+          "featureType": "road.highway",
+          "elementType": "labels",
+          "stylers": [
+            {
+              "visibility": "on"
+            }
+          ]
+        },
+        {
+          "featureType": "road.arterial",
+          "elementType": "labels.text",
+          "stylers": [
+            {
+              "visibility": "on"
+            }
+          ]
+        },
+        {
+          "featureType": "road.local",
+          "elementType": "labels.text",
+          "stylers": [
+            {
+              "visibility": "on"
+            }
+          ]
+        },
+        {}
+      ]
     });
 
     const marker = new google.maps.Marker({
